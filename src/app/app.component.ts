79e37e89b5ef65f13062312723b610ba5857b436
Ellen Woodward <S00222321@atu.ie>
2023-02-21 15:49:23 +0000

    added html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Movie } from './movie.model';
 import { MovieService } from './movie.service';
 
 @Component({
@@ -7,4 +8,9 @@ import { MovieService } from './movie.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public mySelectedMovie!: Movie;
+
+  setSelectedMovie(movie:Movie){
+    this.mySelectedMovie = movie;
+  }
 }
--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from '../movie.model';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.css']
 })
-export class MovieDetailComponent {
+export class MovieDetailComponent implements OnInit{
 
+  @Input()
+  movieDetails!: Movie;
+
+  constructor(){}
+
+  ngOnInit(): void {}
 }
